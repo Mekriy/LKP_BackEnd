6c37036d413bc8079766b9af5e82c514e3f70755
olesosa <oleosad@gmail.com>
2023-10-17 19:23:55 +0300

    added order repos

---
+++ WebAPI_LKP/WebAPI_LKP/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using WebAPI_LKP.Interfaces;
+using WebAPI_LKP.Models;
+
+namespace WebAPI_LKP.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        readonly DbContexts.AppContext context;
+        public OrderRepository(DbContexts.AppContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Order> AllOrders
+        {
+            get
+            {
+                return context.Orders;
+            }
+        }
+
+        public Order? GetOrderById(Guid orderId) 
+        {
+            return AllOrders.FirstOrDefault(o => o.Id == orderId);
+        }
+
+        public double CalculateTotalPrice(Order order) 
+        { 
+            double totalPrice = 0;
+            foreach (var product in order.Products) 
+            {
+                totalPrice += product.Price * product.Count;
+            }
+            return totalPrice;
+        }
+    }
+}
