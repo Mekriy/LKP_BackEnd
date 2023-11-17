d62b38a7416296d38860d01cf25b235b6173c24e
Andrii Pasternak <a.pasternak@nltu.lviv.ua>
2023-11-17 11:00:51 +0200

    Updated connection string

--- WebAPI_LKP/WebAPI_LKP/DbContexts/LkpContext.cs
+++ WebAPI_LKP/WebAPI_LKP/DbContexts/LkpContext.cs
@@ -5,13 +5,6 @@ namespace WebAPI_LKP.DbContexts
 {
     public class LkpContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // Олесь
-            //optionsBuilder.UseMySQL("Server=localhost;database=DzhgutDb;user=root;password=root");
-            //Andrii
-            optionsBuilder.UseMySQL("Server=localhost;database=DzhgutDb;user=root;password=MyPassword54321");
-        }
         public LkpContext(DbContextOptions<LkpContext> options) : base(options) { }
         public LkpContext() { }
         public DbSet<User> Users { get; set; }
